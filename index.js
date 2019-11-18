f7f4084fa5c0e49504e2e2f5d748151155f55b8f
Vinay Lohana <vlohana@sfu.ca>
2019-11-17 22:30:00 -0800

    Added chat room

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const express = require('express');
+var express = require('express');
+var http = require('http');
+var socketIO = require('socket.io');
 const path = require('path');
 const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
@@ -6,13 +8,10 @@ const session = require('express-session')
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser')
 var password_hash = require('password-hash');
+const app = express()
+const server = http.Server(app)
+const io = socketIO(server)
 
-var http = require('http');
-var socketIO = require('socket.io');
-var server = http.Server(app);
-var io = socketIO(server);
-
-var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -55,7 +54,7 @@ app.get('/db', async (req, res) => {
 app.get('/', (req, res, next) => {
     if (req.session.loggedin){
         console.log("logged in");
-       res.render('gameCanvas');
+       res.render('GameCanvas');
     }else{
       next();
     }
@@ -87,122 +86,26 @@ app.post("/:id",(req, res) => {
 
 });
 
-  app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-
-
-  // Class to create tank objects
-
-  class Bullet{
-    constructor(tank){
-      this.width = 10
-      this.height = 4
-      //this.x = where the tank at
-      //this.y = where the tank at
-      // this.maxSpeed = 4
-      this.speed = 8 //discuss for weather condition
-      this.numOfBullet = 5
-      this.x = tank.x;
-      this.y = tank.y + (tank.height/2) - 1.7;
-      this.color = "yellow";
-    }
-
-    draw(ctx)
-    {
-      ctx.fillStyle = this.color;
-      ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-
-    update()
-    {
-      this.x -= this.speed;
-    }
-
-  }
-
-  class Tank{
-    constructor(canvasWidth, canvasHeight)
-    {
-      this.width = 60;
-      this.height = 40;
-      this.x = 0;
-      this.y = 0;
-      this.maxSpeed = 4;
-      this.xSpeed = 0;
-      this.ySpeed = 0;
-      this.bullets = new Bullet(this);
-      this.bulletX = 0;
-    }
-
-    fire(){
-      let bullet = new Bullet(this);
-      this.bullets.push(bullet);
-    }
-
-    stop()
-    {
-      this.xSpeed = 0;
-      this.ySpeed = 0;
-    }
-
-    draw(ctx)
-    {
-      ctx.drawImage(image, this.x, this.y, this.width, this.height);
-
-      for (var i = 0; i<this.bullets.length ; i++){
-        this.bullets[i].draw(ctx);
-      }
-    }
-
-    update(frameRate)
-    {
-      if(!frameRate)
-        return;
-
-      for (var i = 0; i<this.bullets.length ; i++){
-        this.bullets[i].update();
-      }
-
-    }
-  };
-
+  server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
   var players = {};
   let bullet = {};
   io.on('connection', function(socket) {
-    socket.on('new player', function() {
-      players[socket.id] = new Tank();
+    console.log("A user connected")
+    socket.on('username', function(username) {
+        socket.username = username;
+        io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
     });
 
-    socket.on('new bullet', function() {
-      bullet = new Bullet(players[socket.id]);
+    socket.on('disconnect', function(username) {
+        io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
+    })
+
+    socket.on('chat message', function(message, username) {
+        io.emit('chat message', username + ': ' + message);
     });
-    socket.on('movement', function(tankAction) {
-      var player = players[socket.id] || {};
-      if (tankAction.left) {
-          player.x -= 4;
-          if(player.x < 0)
-            player.x = 0;
-      }
-      if (tankAction.up) {
-          player.y -= 4;
-          if(player.y < 0)
-            player.y = 0;
-      }
-      if (tankAction.right) {
-          player.x += 4;
-          if( player.x + player.width > 800 )
-              player.x = 800 - player.width;
-      }
-      if (tankAction.down) {
-          player.y += 4;
-          if( player.y + player.height > 600 )
-              player.y = 600 - player.height;
-      }
-      if(tankAction.shoot){
-          bullet.x -= 8;
-      }
-    });
+
+
   });
 
   setInterval(function() {
@@ -214,6 +117,7 @@ app.post("/:id",(req, res) => {
   function SignIn(req, res){
     var username = req.body.username_login;
     var password = req.body.password_login;
+    app.locals.username = username;
     var check_password_username = `SELECT username, password FROM gamedata WHERE username = '${username}';`;
 
     pool.query(check_password_username,(err,result)=>{
@@ -227,7 +131,7 @@ app.post("/:id",(req, res) => {
           req.session.loggedin = true;
           req.session.username = username;
           console.log("session:  ", req.session);
-          res.render('gameCanvas');
+          res.render('GameCanvas', {username: req.session.username} );
         }else{
           var result = {'rows': result.rows}
           res.render('Home',{ isError:"true"});
